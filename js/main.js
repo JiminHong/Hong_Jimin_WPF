ea625beb3e75eba8b087f674d88d48255b565894
JiminHong <jiminhong@JIMINs-MacBook-Pro.local>
2014-09-08 18:54:29 -0400

    Commit 2
    commit 2

--- js/main.js
+++ js/main.js
@@ -1,17 +1,14 @@
 console.log("Hello");
 
 //declaration and assignment;
-var score=10;
+var x=2;
 var crazy=2;
 
 
 //change an existing variable's value;
 //score=10;
 //using expressions to change variable's value;
-score+=2;
-score+=crazy/2;
-score*=2;
 
-console.log(score);
+x++; // means x+=1;
 
-
+x--; // means x-=1;  
