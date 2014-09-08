b23cdd0f51b83069fac3de233f51f565e0d3586f
JiminHong <jiminhong@JIMINs-MacBook-Pro.local>
2014-09-08 18:51:53 -0400

    Commit 1 for day3
    Commit 1 for day3

---
+++ js/main.js
@@ -0,0 +1,17 @@
+console.log("Hello");
+
+//declaration and assignment;
+var score=10;
+var crazy=2;
+
+
+//change an existing variable's value;
+//score=10;
+//using expressions to change variable's value;
+score+=2;
+score+=crazy/2;
+score*=2;
+
+console.log(score);
+
+
