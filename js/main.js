4231a6ac9426c809dc1cb799efc8c608325b8471
JiminHong <jiminhong@JIMINs-MacBook-Pro.local>
2014-09-08 20:02:20 -0400

    Commit 3 for day3
    Commit 3

--- js/main.js
+++ js/main.js
@@ -1,14 +1,56 @@
 console.log("Hello");
 
 //declaration and assignment;
-var x=2;
-var crazy=2;
+
+
+//review expressions;
+//++ adds 1
+//-- subtracts 1
+//= assignment (this doesn't mean equal anymore.)
+//+= increments;
+//-= decrements;
 
 
 //change an existing variable's value;
 //score=10;
 //using expressions to change variable's value;
 
-x++; // means x+=1;
+//Data Types!!!!!!!
+//var score=5; //Number
+//var name="bob"; //String //score="10"; // "" 이 표시 있으면 string. Even though you put numbers in the quotation mark.
+//var happy=true; //Boolean
+//var grades = [80,90,100,90,100,110]; //Array
 
-x--; // means x-=1;  
+var score=5;
+var firstName="bob";
+var lastName="Smith";
+var fullName=firstName+" "+lastName; //+양쪽이 number가 아니고 + 기호를 쓰는 것. "concatenation"이라고 부른다.
+                                     // concatenate 한다고 말함. You concatenate space.
+var happy=true;
+var grades = [80,90,100,90,100,110];
+
+
+//console.log(grades[0]); // array access notation
+console.log("name",fullName);
+
+
+var total = 5+4;
+total+=3;
+console.log(total); //answer is 543. In this case you are adding 3.
+
+var total = "5"+4;
+total+=3;
+console.log(total); //answer is 543. In this case you are not adding 3 but concatenating 3.
+
+var total = "a"+"b";
+total+="c";
+console.log(total);
+
+var total = "a"+"b";
+total+=1000;
+console.log(total);
+
+
+var total = 35;
+total+=1000;
+console.log(total);
