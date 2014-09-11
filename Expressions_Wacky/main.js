463607dc49e9ef39aab528712e12939ffa5ebc9d
JiminHong <jiminhong@JIMINs-MacBook-Pro.local>
2014-09-10 22:37:28 -0400

    Expression_Wacky_17
    Expression_Wacky_17

--- Expressions_Wacky/main.js
+++ Expressions_Wacky/main.js
@@ -3,3 +3,10 @@
 //Web Programming Fundamentals
 //Section_01
 //Expression_Wacky
+
+
+//My boss required us to find 500 funny images for a project.
+//I am one of the team leaders.
+//We have total five teams.
+//There are 4 team members in one team and there is one team leader in one team.
+//If all people need to find same amount of images, how many images do I need to find?
