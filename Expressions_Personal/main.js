d38f8984d2014091a67d950e4699da825c5c8574
JiminHong <jiminhong@JIMINs-MacBook-Pro.local>
2014-09-10 21:38:43 -0400

    Expression_Personal_07
    Expression_Personal_07

--- Expressions_Personal/main.js
+++ Expressions_Personal/main.js
@@ -8,7 +8,7 @@
 //How long I should work part-time if I would like to wear new t-shirts for every new classes?
 
 //I can earn $10 per an hour.
-var hour = prompt("How many hours do I need to work?");
+
 var money = prompt("How much can I earn per one hour?");
 
 //How much I will spend if I wear different t-shirts everyday and buy new one every time. T-shirt is $10 each.
@@ -26,11 +26,17 @@ var times = prompt ("How many times do I need to go for one class?");
 var totalClassesTime = classes*times;
 console.log(totalClassesTime);
 
-console.log("I have "+classes+" "+"class(es) in this month and I have to go " +times+ "times in one class."
-"Therefore, I have to purchase "+totalClassesTime+" "+"t-shirts in this month." );
+console.log("I have "+classes+" "+"class(es) in this month and I have to go " +times+ " times in one class. Therefore, I have to purchase "+totalClassesTime+" "+"t-shirts in this month.");
 
 
 //If I need 11 t-shirts, how much do I need to buy them?
 var totalMoney = totalClassesTime*priceTshirt;
 
 console.log("Since a t-shirt is $"+priceTshirt+", I need $" +totalMoney+".")
+
+//How many hours do I need to work?
+
+var totalHours = totalClassesTime*priceTshirt/money;
+
+console.log("Since I can earn $"+money+" per one hour, I need to work "+totalHours+" hours to get all of them.")
+
