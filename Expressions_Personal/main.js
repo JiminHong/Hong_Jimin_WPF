ac1177ddba60b5b43f608c1895e6aa6cf0cdfe4e
JiminHong <jiminhong@JIMINs-MacBook-Pro.local>
2014-09-10 21:14:06 -0400

    Expression_Personal_02
    Expression_Personal_02

--- Expressions_Personal/main.js
+++ Expressions_Personal/main.js
@@ -7,5 +7,12 @@
 
 //How long I should work part-time if I would like to wear new t-shirts for every new classes?
 //I can earn $10 per an hour.
+
+var hour = prompt("How many hours do I need to work?")
+
 //How much I will spend if I wear different t-shirts everyday and buy new one every time. T-shirt is $10 each.
+
+var priceTshirt = prompt("How much is the t-shirt?")
+
 //I have 11 classes in this month.
+
