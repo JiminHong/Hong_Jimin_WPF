2908e89bac2ea16c96efa91d08178410cfa7b17b
JiminHong <jiminhong@JIMINs-MacBook-Pro.local>
2014-09-10 21:55:57 -0400

    Expression_Personal_08
    Expression_Personal_08

--- Expressions_Personal/main.js
+++ Expressions_Personal/main.js
@@ -14,11 +14,13 @@ var money = prompt("How much can I earn per one hour?");
 //How much I will spend if I wear different t-shirts everyday and buy new one every time. T-shirt is $10 each.
 var priceTshirt = prompt("How much is the t-shirt?");
 
-//I have 1 class in this month.
-var classes = prompt ("How many classes do I have in this month?");
+//I have 2 class in this month.
+
+var classes = prompt("What classes do you have in this month?");
+
 
 //I have to go 11 times per one class in this month.
-var times = prompt ("How many times do I need to go for one class?");
+var times = prompt("How many times do I need to go for one class in this month?");
 
 
 
@@ -26,17 +28,16 @@ var times = prompt ("How many times do I need to go for one class?");
 var totalClassesTime = classes*times;
 console.log(totalClassesTime);
 
-console.log("I have "+classes+" "+"class(es) in this month and I have to go " +times+ " times in one class. Therefore, I have to purchase "+totalClassesTime+" "+"t-shirts in this month.");
+console.log("I have "+classes+" in this month and I have to go " +times+ " times in one class. Therefore, I have to purchase "+totalClassesTime+" "+"t-shirts in this month.");
 
 
 //If I need 11 t-shirts, how much do I need to buy them?
 var totalMoney = totalClassesTime*priceTshirt;
 
-console.log("Since a t-shirt is $"+priceTshirt+", I need $" +totalMoney+".")
+console.log("Since a t-shirt is $"+priceTshirt+", I need $" +totalMoney+".");
 
 //How many hours do I need to work?
-
 var totalHours = totalClassesTime*priceTshirt/money;
 
-console.log("Since I can earn $"+money+" per one hour, I need to work "+totalHours+" hours to get all of them.")
+console.log("Since I can earn $"+money+" per one hour, I need to work "+totalHours+" hours to get all of them.");
 
