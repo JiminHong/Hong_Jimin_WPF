afc6136c2bd902fca4fa01caa5e4e2ba69330ecd
JiminHong <jiminhong@JIMINs-MacBook-Pro.local>
2014-09-12 22:41:16 -0400

    Commit 05
    Commit 05

--- Conditionals_worksheet/js/expressions_with_conditionals.js
+++ Conditionals_worksheet/js/expressions_with_conditionals.js
@@ -8,50 +8,26 @@
 
 
 
-
+//What is the current temperature?
 var degree = prompt("What is the current temperature?");
 
+//What temperature type do you want to use?
 var tempType = prompt("Do you want to convert it to Celsius or Fahrenheit?");
 
+
+//Converting Fahrenheit to Celsius
 convertedTempFtoC = (degree-32)/1.8;
+//Converting Celsius to Fahrenheit
 convertedTempCtoF = 1.8*degree+32;
 
-
+//If you want to convert from Celsius to Fahrenheit
 if(tempType === "Fahrenheit"){
     //Celsius to Fahrenheit
 
     console.log("The temperature is "+convertedTempCtoF+"F.");
+
 }else if(tempType === "Celsius"){
     //Fahrenheit to Celsius
 
     console.log("The temperature is "+convertedTempFtoC+"C.");
 }
-
-
-
-/*
-if(convertUnit = "celsius"){
-
-    convertedTempFtoC = (degree-32)/1.8;
-    console.log("The temperature is "+convertedTempFtoC+"C.");
-
-}else if(converUnit = "Fahrenheit"){
-    convertedTempCtoF = (1.8*degree)+32;
-    console.log("The temperature is "+convertedTempCtoF+"F.");
-}
-*/
-
-
-/*
-var tempType = "celsius";
-
-if(tempType = "celsius"){
-    convertedTempFtoC = (degree-32)/1.8;
-    convertedTempCtoF = (1.8*degree)+32;
-    console.log("The temperature is " + convertedTempFtoC);
-
-}else{
-    console.log("The temperature is "+degreeCelsius+"C.");
-}
-
-*/
