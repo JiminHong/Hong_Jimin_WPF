ddf4dad6cecd2a8e6baeaa2866f97cc2197e4aa3
JiminHong <jiminhong@JIMINs-MacBook-Pro.local>
2014-09-12 23:07:56 -0400

    Commit 06
    Commit 06

--- Conditionals_worksheet/js/multiple_results.js
+++ Conditionals_worksheet/js/multiple_results.js
@@ -1,3 +1,11 @@
-/**
- * Created by jiminhong on 9/12/14.
- */
+//Jimin Hong
+//9/12/2014
+//Web Programming Fundamentals
+//Section_00
+//Conditionals worksheet
+//Multiple Results
+
+//ASKing username and password
+var usernameByUser = prompt("What is your username?");
+var passwordByUser = prompt("What is your password?");
+
