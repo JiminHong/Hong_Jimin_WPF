e34a09fbd1a1b2af1b295eaf3196c55f5e9d413f
JiminHong <jiminhong@JIMINs-MacBook-Pro.local>
2014-09-12 22:20:49 -0400

    Commit 02
    Commit 02

--- Conditionals_worksheet/js/expressions_with_conditionals.js
+++ Conditionals_worksheet/js/expressions_with_conditionals.js
@@ -5,18 +5,35 @@
 //Conditionals worsheet
 // Expressions with conditionals
 
-//Given degree is Fahrenheit
+
+
+
+
 var degree = prompt("What is the current temperature?");
-var convertedTemp;
 
-//var degreeCelsius = (degree-32)/1.8;
+var unit = prompt("Do you want to convert it to Celsius or Fahrenheit?")
 
+if(unit = "celsius"){
+    convertedTempFtoC = (degree-32)/1.8;
+    console.log("The temperature is "+convertedTempFtoC+"C.");
+
+}else if(unit = "Fahrenheit"){
+    convertedTempCtoF = (1.8*degree)+32;
+    console.log("The temperature is "+convertedTempCtoF+"F.");
+}
+
+
+
+/*
 var tempType = "celsius";
 
 if(tempType = "celsius"){
-    convertedTemp = (degree-32)/1.8;
-    console.log("The temperature is " + convertedTemp);
+    convertedTempFtoC = (degree-32)/1.8;
+    convertedTempCtoF = (1.8*degree)+32;
+    console.log("The temperature is " + convertedTempFtoC);
 
 }else{
     console.log("The temperature is "+degreeCelsius+"C.");
 }
+
+*/
