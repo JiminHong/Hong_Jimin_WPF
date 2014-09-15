d08cd69e88bb4ef9c92871c82b7c01c167dfac51
JiminHong <jiminhong@JIMINs-MacBook-Pro.local>
2014-09-14 22:33:15 -0400

    Commit 12
    Commit 12

--- Conditionals_worksheet/js/multiple_results.js
+++ Conditionals_worksheet/js/multiple_results.js
@@ -5,27 +5,32 @@
 //Conditionals worksheet
 //Multiple Results
 
-//ASKing username and password
+//Asking username
 var usernameByUser = prompt("What is your username?");
+//Asking password
 var passwordByUser = prompt("What is your password?");
 
-//Given username and password
+//Given username
 var username = "ABC";
+//Given password
 var password = "123";
 
 
 
 // when username and password are matched.
 if(usernameByUser === username && passwordByUser === password){
+    //when user name and password are matched.
     console.log("welcome, "+username+"!");
 }
 
 //When your user name does not match.
 else if (usernameByUser != username && passwordByUser === password){
+    //when your username is not correct
     console.log("User not found. Try again.");
 }
 
 //Password doesn't match our records.
 else{
+    //when your password is not correct.
     console.log("Password does not match our records.");
 }
