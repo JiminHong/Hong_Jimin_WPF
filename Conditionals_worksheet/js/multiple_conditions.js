278ce42e8840fea17487f1b773cd8773bf66241e
JiminHong <jiminhong@JIMINs-MacBook-Pro.local>
2014-09-14 22:31:21 -0400

    Commit 11
    Commit 11

--- Conditionals_worksheet/js/multiple_conditions.js
+++ Conditionals_worksheet/js/multiple_conditions.js
@@ -1,7 +1,25 @@
+//Jimin Hong
+//9/12/2014
+//Web Programming Fundamentals
+//Section_00
+//Conditionals worksheet
+//Multiple Conditions
 
 
 //What time are you going to watch the movie?
 var time = prompt("What time do you want to watch the movie?");
 
 //Age of the customer?
-var age = prompt("How old are you?")
+var age = prompt("How old are you?");
+
+
+
+
+if(55 <= age || 10 >= age){
+    console.log("Your ticket price is $7.00");
+}else if (3 <= time && 5 >= time){
+    console.log("Your ticket price is $7.00");
+}else{
+    console.log("Your ticket price is $12.00");
+}
+
