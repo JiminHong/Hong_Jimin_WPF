9fc418e5450d2b9f37abb28994954e32438feb44
JiminHong <jiminhong@JIMINs-MacBook-Pro.local>
2014-09-15 22:19:03 -0400

    commit 10
    commit 10

--- Conditionals_Industry/js/main.js
+++ Conditionals_Industry/js/main.js
@@ -1,3 +1,7 @@
-/**
- * Created by jiminhong on 9/15/14.
- */
+// Jimin's web design company
+// client will enter the number of pages they want to build.
+// there is a minimum price
+// there is a maximum number of pages we can build.
+// we will charge $60 per a page after 10th pages.
+
+// alert("Hello! Welcome to Jimin's web design company!");
