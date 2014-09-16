c8324d3d46398a3fe66bfa4aaadadb28baaf2456
JiminHong <jiminhong@JIMINs-MacBook-Pro.local>
2014-09-15 22:30:01 -0400

    commit 14
    commit 14

--- Conditionals_Industry/js/main.js
+++ Conditionals_Industry/js/main.js
@@ -22,3 +22,12 @@ if(pages > 0 && pages <= 10){
 
     console.log ("You are trying to make a basic website. Whether you want only 1 pages or 10 pages, the minimum price is $500.");
 }
+
+
+//if the client wants to build more than 10 pages less or equal than 200 pages.
+else if (pages > 10 && pages <= 200){
+
+    var totalPrice = (pages-10)*60+500;
+
+    console.log ("Minimum price for 10 pages is $500. Over 10 pages, we will charge $60 per a page. Since you would like to make "+pages+" pages, your total is $"+totalPrice+".");
+}
