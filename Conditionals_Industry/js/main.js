3da71b0155671fc09e101fb52f6f732b14a99b63
JiminHong <jiminhong@JIMINs-MacBook-Pro.local>
2014-09-15 22:27:41 -0400

    commit 13
    commit 13

--- Conditionals_Industry/js/main.js
+++ Conditionals_Industry/js/main.js
@@ -10,8 +10,15 @@ alert("Hello! Welcome to Jimin's web design company!");
 var pages = prompt("How many pages do you want to build for your website?");
 
 
-//if the client put nothing 
+//if the client put nothing
 if(pages === ""){
 
     pages = prompt ("PLEASE enter a number and do not leave blank");
+}
+
+
+// if the client wants to build less than 10 pages.
+if(pages > 0 && pages <= 10){
+
+    console.log ("You are trying to make a basic website. Whether you want only 1 pages or 10 pages, the minimum price is $500.");
 }
