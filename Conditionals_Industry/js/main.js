24abd061970cd87161464df10ebb4c4daa8269a0
JiminHong <jiminhong@JIMINs-MacBook-Pro.local>
2014-09-15 22:22:36 -0400

    commit 11
    commit 11

--- Conditionals_Industry/js/main.js
+++ Conditionals_Industry/js/main.js
@@ -4,4 +4,8 @@
 // there is a maximum number of pages we can build.
 // we will charge $60 per a page after 10th pages.
 
-// alert("Hello! Welcome to Jimin's web design company!");
+alert("Hello! Welcome to Jimin's web design company!");
+
+//ask how many pages do they want?
+var pages = prompt("How many pages do you want to build for your website?");
+
