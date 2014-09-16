2a50a34dee8b02b0043c6856d583ac614669f54b
JiminHong <jiminhong@JIMINs-MacBook-Pro.local>
2014-09-15 21:20:23 -0400

    Commit 2
    commit 2

--- Conditionals_Personal/js/main.js
+++ Conditionals_Personal/js/main.js
@@ -2,3 +2,8 @@
 // However, because of budget I have to pick how many souvenir I will get.
 // I want to get souvenirs at least for my family.
 
+//how many family member do you have?
+
+var min = prompt("How many family members do you have?");
+
+var max = prompt("How many friends do you have?");
