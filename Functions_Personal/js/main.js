cfea16bae17ec113d50c056fe58b4d6a78d88c95
JiminHong <jiminhong@JIMINs-MacBook-Pro.local>
2014-09-22 18:32:11 -0400

    commit 4
    commit 4

--- Functions_Personal/js/main.js
+++ Functions_Personal/js/main.js
@@ -8,3 +8,19 @@ var classes = prompt("How many classes do you have in one course?");
 
 //How many hours do I have to take for one class?
 var hour = prompt("How many hours do you have in one class?");
+
+/*
+//anonymous function
+
+var functionName = function(parameters){
+
+    //code to execute;
+    //return value;
+};
+
+functionName(arguments);
+*/
+
+//totalHours is a function name.
+//parameter c = classes , h = hour
+var totalHours = function (c,h)
