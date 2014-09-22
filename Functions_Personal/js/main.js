159706f92629e4a0d179c5199d87ccff2d2cead1
JiminHong <jiminhong@JIMINs-MacBook-Pro.local>
2014-09-22 18:30:13 -0400

    commit 3
    commit 3

--- Functions_Personal/js/main.js
+++ Functions_Personal/js/main.js
@@ -2,6 +2,9 @@
 //I have only one class in this month
 //Prompts!
 
+//How many classes do I have?
 var classes = prompt("How many classes do you have in one course?");
 
+
+//How many hours do I have to take for one class?
 var hour = prompt("How many hours do you have in one class?");
