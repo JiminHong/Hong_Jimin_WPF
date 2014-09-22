dc5a500073db951738e58816d41d5e79fd40874e
JiminHong <jiminhong@JIMINs-MacBook-Pro.local>
2014-09-22 19:42:28 -0400

    commit 12
    commit 12

--- Functions_Wacky/js/main.js
+++ Functions_Wacky/js/main.js
@@ -9,8 +9,13 @@ var girlNames = ["Alisa","Susan","Kimberly"];
 alert("You have to make a girl name for your friend. We have Alisa, Susan and Kimberly so far.")
 
 //Adding a name you recommend.
-var girlNames.push("Can you recommend a girl name besides what I said before? Please enter a name.");
+var nameOne = prompt("Can you recommend a girl name besides what I said before? Please enter a name.");
 
+
+//Another name!
+girlNames.push = prompt("Actually, I don't think your friend would like the name ' "+nameOne+" '. Please enter another girl name for your friend.");
+
+console.log(girlNames);
 
 
 
