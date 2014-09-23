9a8fe2e65818e26ffd9de070ea32abb9f6288df8
JiminHong <jiminhong@JIMINs-MacBook-Pro.local>
2014-09-22 20:44:39 -0400

    commit 15
    commit 15

--- Functions_Industry/js/main.js
+++ Functions_Industry/js/main.js
@@ -1,3 +1,27 @@
+//you have to have at least 100 square pixels image to have a great background in your website
+
+
+//Asking the height and width of the image I picked?
+
+var height = prompt("What is the height of your image?");
+
+var width = prompt("What is the width of your image?");
+
+var a = imageArea(height,width);
+
+
+console.log(a);
+
+function triangleArea(b,h){
+
+    var area = .5*b*h;
+    return area;
+}
+
+
+
+
+
 var gpa = 3.0;
 
 //if GPA is over the min 2.0 score, then student can graduate
