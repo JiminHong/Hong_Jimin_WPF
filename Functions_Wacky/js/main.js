af9867f434b15f4eed13503f6b8d90a6fead0c21
JiminHong <jiminhong@JIMINs-MacBook-Pro.local>
2014-09-22 20:38:26 -0400

    commit 14
    commit 14

--- Functions_Wacky/js/main.js
+++ Functions_Wacky/js/main.js
@@ -12,16 +12,33 @@ alert("You have to make a girl name for your friend. We have Alisa, Susan and Ki
 var nameOne = prompt("Can you recommend a girl name besides what I said before? Please enter a name.");
 
 
-if (nameOne === girlNames[0] || nameOne === "alisa"|| nameOne === girlNames[1] || nameOne === "susan" || nameOne === girlNames[2] || nameOne === "kimberly"){
-    alert("Please enter a name besides Alisa, Susan, and Kimberly.");
+//Ask until the user enter a name besides given names.
+
+while (nameOne === girlNames[0] || nameOne === girlNames[1] || nameOne === girlNames[2]){
+    var nameOne = prompt ("Please enter a name besides Alisa, Susan, and Kimberly.")
 }
 
+
+
+if (nameOne === girlNames[0] || nameOne === girlNames[1] || nameOne === girlNames[2]){
+    alert("Please enter a name besides Alisa, Susan, and Kimberly.");
+
+    girlNames.push(prompt("Please enter another name."))
+
+
+}else if(nameOne === ""){
+
+    girlNames.push(prompt("Please enter a name and do not leave a blank."))
+}
+
+
+else{
+
 //Another name!
-girlNames.push = prompt("Actually, I don't think she would like the name ' "+nameOne+" '. Please enter another girl name for your friend.");
+
+    girlNames.push(prompt("Actually, I don't think she would like the name ' " + nameOne + " '. Please enter another girl name for your friend."));
+}
+
 
 console.log(girlNames);
-
-
-
-
 
