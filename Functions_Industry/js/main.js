75e4c93610a4eb5886d7674c225776a53bf844f7
JiminHong <jiminhong@JIMINs-MacBook-Pro.local>
2014-09-22 20:52:34 -0400

    commit 20
    commit 20

--- Functions_Industry/js/main.js
+++ Functions_Industry/js/main.js
@@ -3,16 +3,18 @@
 
 //Asking the height and width of the image I picked?
 
+//What is the height of the image?
 var height = prompt("What is the height of your image?");
 
+//what is the width of the image?
 var width = prompt("What is the width of your image?");
+
 
 var a = imageArea(height,width);
 
 
 
-//Normal function
-
+//Normal named function
 
 console.log("Area of your image is "+a+".");
 
@@ -20,15 +22,12 @@ console.log("Area of your image is "+a+".");
 //Function name and functions
 function imageArea(h,w){
 
-    //Calculate the area of the rectangle
+    //Calculate the area of the rectangle/ code to execute
     var areaOfImage = h*w;
 
     //return it
     return areaOfImage;
 }
-
-
-
 
 
 //(condition to be met) ? first set of stuff : else stuff;
